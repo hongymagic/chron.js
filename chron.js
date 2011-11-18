9873ca72fc45197ab8f8dec0a65a55e6e47bcdd8
David Hong <davidhong.code@gmail.com>
2011-11-18 11:04:25 +1100

    new 2.0 work

--- chron.js
+++ chron.js
@@ -1,148 +1,84 @@
 
 /*jslint node: true, indent: 2, browser: true, es5: true, plusplus: true */
-/**
- * chron.js - a time-based storage interface
- *
- * @requires localStorage
- *
- * Without localStorage support, it will use inline JSON object as storage
- * and will not persist;
- */
-var chron = (function (global) {
-  'use strict';
+
+
+
+
+
+
+var Chron = (function (global) {
+
+
 
   var 
-
-    supportsStorage = (function () {
+  
+    supportsStorage = function () {
       try {
-        return !!global.localStorage;
+        return !!global.localStorage.getItem;
       } catch (e) {
         return false;
       }
-    }()),
-    localStorage = global.localStorage,
+    }(),
 
-    serialize = function (object) {
-      return JSON.stringify(object);
-    },
-    deserialize = function (serial) {
-      return JSON.parse(serial);
-    },
+// 
+// Simple stack data structure
 
-    /**
-     * Custom storage object
-     *
-     * {
-     *  time1: { object }
-     *  ..
-     *  tineN: { object }
-     * }
-     */
-    _store = supportsStorage ? deserialize(localStorage.getItem('chron.js')) : {},
-    _getItem = function (key) {
-      return _store[key];
-    },
-    _setItem = function (value) {
-      var timestamp = +(new Date());
-      _store[timestamp] = value;
-
-      if (supportsStorage) {
-        localStorage.setItem('chron.js', serialize(_store));
-      }
-
-      return timestamp;
-    },
-    _clear = function () {
-      _store = {};
-
-      if (supportsStorage) {
-        localStorage.setItem('chron.js', serialize(_store));
-      }
-    },
-
-    /**
-     * Returns all times of all snapshots captured by chron.js. Time are 
-     * sorted in descending order (more recent first)
-     *
-     * @private
-     * @returns {Array} of times (in milliseconds after the epoch)
-     */
-    _timeline = function () {
-      var 
-      
-        timeline = Object.keys(_store),
-        desc = function (a, b) {
-          return Number(b) - Number(a);
-        };
-
-// Sort given times in descending order
-
-      timeline = timeline.sort(desc);
-      return timeline;
+    Stack = function (size) {
+      this.size   = (size | 0);
+      this._store = [];
     };
 
-  if ('undefined' === typeof _store || _store === null) {
-    _store = {};
-  }
+  Stack.prototype.push = function (object) {};
+  Stack.prototype.pop = function (count) {};
+
 
   return {
     /**
-     * Take a snapshot of the given value
+     * take a snapshot of an object
      *
-     * @param   value {Object}  Object to snapshot
-     * @returns       {Number}  Time of the snap shot in milliseconds since epoch
-     * @example:
-     *  chron.snap($form)
+     * @param   {Object}  object to store in timeline
+     * @returns {Date}    timestamp in milliseconds since the Unix epoch
      */
-    snap: function (value) {
-      return _setItem(value);
+    snap: function (object) {
     },
 
     /**
-     * Return list of recent snapshopts. Use count to specify how many recent
-     * snapshots to return;
+     * retrieve number of snapshots in a sorted array, in descending order
      *
-     * @param count {Number}  How many items to return
-     * @returns     {Array}   List of recent snapshots
-     * @example:
-     *  chron.list(5);
+     * @param   {Number}  number of objects to return
+     * @returns {Array}   array of boxed objects that contain timestamp as property
      */
-    list: function (count) {
-      count = (+count) | 0;
-      if (count === 0) { 
-        return [];
-      }
-
-      var 
-      
-        result = [],
-        timeline = _timeline(),
-        index,
-        length = count > timeline.length ? timeline.length : count;
-
-// Now with sorted timeline, access value componet of the localStorage for
-// given times and return them
-
-      for (index = 0; index < length; index += 1) {
-        result.push(_getItem(timeline[index]));
-      }
-
-      return result;
-    },
-
-    /**
-     * Reset and clear all snapshots captured by chron.js
-     */
-    reset: function () {
-      _clear();
+    timeline: function () {
     }
   };
-}(this));
+})(this));
 
 //
-// Export this module
+// check for module support
 
-if ('undefined' !== typeof module) {
-  module.exports = chron;
+if (module.exports) {
+  module.exports = Chron;
 }
+
+
+//
+// [
+//  {
+//    timestamp: +new Date,
+//    value: object
+//  }
+//  ...
+// ]
+//
+
+
+
+
+
+
+
+
+
+
+
 
