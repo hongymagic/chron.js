ef4b57f8f0f0b3efd53d318ce05c8773eb47e295
David Hong <davidhong.code@gmail.com>
2011-11-29 08:18:53 +1100

    directory rejiggling

---
+++ src/chron.js
@@ -0,0 +1,89 @@
+
+/*jslint node: true, indent: 2, browser: true, es5: true */
+(function (global) {
+  'use strict';
+
+  var
+
+    localStorage = global.localStorage,
+    supportsStorage = !!localStorage,
+    key = function (name) {
+      return 'chron.js:' + name;
+    },
+
+    backup = function (name, data) {
+      if (!supportsStorage) {
+        return data;
+      }
+
+      localStorage.setItem(key(name), JSON.stringify(data));
+      return data;
+    },
+    restore = function (name) {
+      if (!supportsStorage) {
+        return [];
+      }
+
+      return JSON.parse(localStorage.getItem(key(name)));
+    },
+
+    Chron;
+
+  Chron = function (name) {
+    if (!(this instanceof Chron)) {
+      return new Chron(name);
+    }
+
+// set the instance name, restore data from localStorage
+
+    this.name = name;
+    this.store = restore(name);
+  };
+
+  Chron.prototype.snap = function () {
+    var 
+
+// we snap each argument, one by one
+
+      args = [].slice.call(arguments),
+      index, 
+      length;
+
+    for (index = 0, length = args.length; index < length; index += 1) {
+      this.store.push({
+        timestamp: +(new Date()),
+        value: args[index]
+      });
+    }
+
+    backup(this.name, this.store);
+  };
+
+  Chron.prototype.list = function (count) {
+    /* defaults */
+    if (typeof count !== 'number') {
+      count = 5;
+    }
+
+    var
+
+      length = this.store.length,
+      index = length - count;
+
+    return this.store.slice(index, length).reverse();
+  };
+
+  Chron.prototype.clear = function () {
+    delete this.store;
+    this.store = [];
+
+    backup(this.name, this.store);
+  };
+
+  global.Chron = Chron;
+
+  if (typeof module !== 'undefined') {
+    module.exports = Chron;
+  }
+}(this));
+
